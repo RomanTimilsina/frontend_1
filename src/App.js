f0243cb08a035a34ec05484f1deab3af407919f0
Roman <romantimilsina@gmail.com>
2023-03-12 19:34:45 +0545

    made_basic_app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './App.css';
+import Add from "./component/Add";
+import Book from "./component/Book";
+import Update from "./component/Update";
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/books",
+      element: <Book/>,
+    },
+    {
+      path: "/add",
+      element: <Add/>,
+    },
+    {
+      path: "/update",
+      element: <Update/>,
+    },
+  ]);
+  
+  return (
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
+
